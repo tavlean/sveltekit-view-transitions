14783e683f1ba6cdf10bb0e414c4695d8da7f71a
T.A.V <71072795+tavlean@users.noreply.github.com>
2025-01-09 01:24:21 +0530

    Debugging

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,9 @@
-import adapter from '@sveltejs/adapter-static'
-import { vitePreprocess } from '@sveltejs/kit/vite'
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter(),
-	},
-}
-
-export default config
+export default {
+    kit: {
+        adapter: adapter(),
+    },
+    preprocess: vitePreprocess(),
+};
